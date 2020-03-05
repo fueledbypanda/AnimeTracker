d84859859d06d788277293c8764a45c6b8f5ee6b
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-03-04 21:21:00 -0500

    thanks travis

---
+++ src/db.js
@@ -0,0 +1,12 @@
+const pg = require("pg");
+// const client = new pg.Client(
+//   process.env.DATABASE_URL || "postgres://localhost/acme_db"
+// );
+
+// client.connect();
+
+const sync = async () => {};
+
+module.exports = {
+  sync
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const root = document.querySelector("#root");
+
+const App = () => {
+  return <div>test</div>;
+};
+ReactDom.render(<App />, root);
