119717ca159329ff9b6204c8e71ef69e1873e66c
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-03-07 15:07:23 -0500

    track axios@0.19.2 in package.json

--- src/components/CardTemplate.js
+++ src/components/CardTemplate.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CardTemplate({ anime }) {
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img src={anime.img_url} class="card-img-top" alt="..." />
+      <img src={anime.image_url} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{anime.title}</h5>
         <p className="card-text">Start date: {anime.start_date}</p>
